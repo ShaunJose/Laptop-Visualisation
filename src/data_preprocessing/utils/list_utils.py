539360a241676c2959ad5dbc813c8127f6162523
ShaunJose <shaun.jose12@gmail.com>
2020-12-23 16:04:48 +0000

    Added comments

--- src/data_preprocessing/utils/list_utils.py
+++ src/data_preprocessing/utils/list_utils.py
@@ -1,9 +1,16 @@
 # @author: Shaun
 
 def __get_indexes(list, elements):
+    """
+    Gets indexes of elements in the list
 
+    :param list: List in which elements' indexes need to be found
+    :param elements: List of elements' values whose indexes need to be found
+    :return: List of indexes of elements in the list
+    """
+
+    # Iterate through elements and get indexes of elements in the list
     indexes = []
-
     for elem in elements:
         elem_index = list.index(elem)
         indexes.append(elem_index)
@@ -12,8 +19,15 @@ def __get_indexes(list, elements):
 
 
 def __filter_list(list, indexes):
+    """
+    Trim list by only including indexes needed
 
-    # Add contents of list from indexes given
+    :param list: List to be trimmed
+    :param indexes: Indexes of values needed from the list
+    :return: List with values from indexes given
+    """
+
+    # Iterate through indexes and add those values to a filtered list
     filtered_list = []
     for index in indexes:
         filtered_list.append(list[index])
@@ -22,11 +36,19 @@ def __filter_list(list, indexes):
 
 
 def filter_2d_list(two_d_list, all_fields, wanted_fields):
+    """
+    Trims a 2-dimensional list to get fields wanted
 
-    # get indexes of fields meeded
+    :param two_d_list: The 2-dimensional list to be trimmed
+    :param all_fields: List of fields in each list within the 2-d list
+    :param wanted_fields: List of fields wanted from all the fields in the 2-d list
+    :return: A filtered 2-d list containing lists with elements of only the wanted fields
+    """
+
+    # Get indexes of fields meeded
     wanted_indexes = __get_indexes(all_fields, wanted_fields)
 
-    # append the needed fields to a new 2-dimensional list
+    # Append the wanted fields to a new 2-dimensional list
     filtered_2d_list = []
     for curr_list in two_d_list:
         filtered_list = __filter_list(curr_list, wanted_indexes)
